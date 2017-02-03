e70740bb332ff54166b5d25b5e67078a47c594a9
Santiago Torres <torresariass@gmail.com>
2017-02-02 19:49:39 -0500

    ENH: unicode is borken

--- plugin.py
+++ plugin.py
@@ -889,7 +889,7 @@ class DuckHunt(callbacks.Plugin):
                                 self.scores[currentChannel] = {} 
                                 self.scores[currentChannel][msg.nick] = 1
 
-                        irc.reply(u"♥♥\_o<♥♥ %s: %i (%.2f seconds)" % (msg.nick,  self.scores[currentChannel][msg.nick], befdelay))
+                        irc.reply(u"<3 <3 \_o< <3 <3 %s: %i (%.2f seconds)" % (msg.nick,  self.scores[currentChannel][msg.nick], befdelay))
 
                         self.averagetime[currentChannel] += befdelay
 
